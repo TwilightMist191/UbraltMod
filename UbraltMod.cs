b477125ef3b3b460f6ca20d8afec7c69e7254494
TwilightMist191 <branden.papworth@gmail.com>
2021-02-12 18:59:26 +0000

    Building mod base

---
+++ UbraltMod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Graphics;
+using Terraria;
+using Terraria.GameContent.Dyes;
+using Terraria.GameContent.UI;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.UI;
+
+namespace UbraltMod
+{
+    public class UbraltMod : Mod
+    {
+        public UbraltMod()
+        {
+
+        }
+    }
+}
